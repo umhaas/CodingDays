2cd58b678cf7c7776a843ab2a00691de2b9ad81b
Marat <1874024+vanilla-thunder@users.noreply.github.com>
2021-04-22 18:14:09 +0200

    Reports für Umsätze und Bestellwert

--- Dashboard/src/Report/DataType/Report.php
+++ Dashboard/src/Report/DataType/Report.php
@@ -55,4 +55,97 @@ final class Report
 
         return $data->fetchOne();
     }
+
+    /**
+     * @Field
+     */
+    public function valueMin(): string
+    {
+        $qb = $this->qbfi->create();
+        $data = $qb->select("MIN(oxtotalordersum)")
+            ->from("oxorder")
+            ->where("oxorderdate >= :date")
+            ->setParameter("date", date("Y-m-d"))
+            ->execute();
+
+        return $data->fetchOne();
+    }
+
+    /**
+     * @Field
+     */
+    public function valueAvg(): string
+    {
+        $qb = $this->qbfi->create();
+        $data = $qb->select("AVG(oxtotalordersum)")
+            ->from("oxorder")
+            ->where("oxorderdate >= :date")
+            ->setParameter("date", date("Y-m-d"))
+            ->execute();
+
+        return $data->fetchOne();
+    }
+
+    /**
+     * @Field
+     */
+    public function valueMax(): string
+    {
+        $qb = $this->qbfi->create();
+        $data = $qb->select("MAX(oxtotalordersum)")
+            ->from("oxorder")
+            ->where("oxorderdate >= :date")
+            ->setParameter("date", date("Y-m-d"))
+            ->execute();
+
+        return $data->fetchOne();
+    }
+
+    /**
+     * @Field
+     */
+    public function revenue(): string
+    {
+        $qb = $this->qbfi->create();
+        $data = $qb->select("SUM(oxtotalordersum)")
+            ->from("oxorder")
+            ->where("oxorderdate >= :date")
+            ->andWhere("oxfolder = 'ORDERFOLDER_NEW'")
+            ->setParameter("date", date("Y-m-d"))
+            ->execute();
+
+        return $data->fetchOne() ?? "0";
+    }
+
+    /**
+     * @Field
+     */
+    public function revenuePaid(): string
+    {
+        $qb = $this->qbfi->create();
+        $data = $qb->select("SUM(oxtotalordersum)")
+            ->from("oxorder")
+            ->where("oxorderdate >= :date")
+            ->andWhere("oxpaid != '0000-00-00 00:00:00'")
+            ->setParameter("date", date("Y-m-d"))
+            ->execute();
+
+        return $data->fetchOne() ?? "0";
+    }
+
+    /**
+     * @Field
+     */
+    public function revenueUnpaid(): string
+    {
+        $qb = $this->qbfi->create();
+        $data = $qb->select("SUM(oxtotalordersum)")
+            ->from("oxorder")
+            ->where("oxorderdate >= :date")
+            ->andWhere("oxpaid = '0000-00-00 00:00:00'")
+            ->setParameter("date", date("Y-m-d"))
+            ->execute();
+
+        return $data->fetchOne() ?? "0";
+    }
 }
