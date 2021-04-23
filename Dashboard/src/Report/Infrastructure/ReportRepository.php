3a22186cab5bca60acea8e7d3bd6e2d180ae4522
Unit M - JH <13018499+umhaas@users.noreply.github.com>
2021-04-23 11:28:15 +0200

    Update ReportRepository.php
    Added reportTopProductsByDateDiff to query top sold products in date range

--- Dashboard/src/Report/Infrastructure/ReportRepository.php
+++ Dashboard/src/Report/Infrastructure/ReportRepository.php
@@ -109,4 +109,30 @@ final class ReportRepository
 
         return (int)$queryBuilder->execute()->fetchOne();
     }
+
+    /**
+     * @param DateFilter $dateBetween
+     * @param int        $limit
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function getReportTopProductsByDateDiff(DateFilter $dateBetween, int $limit): array
+    {
+        $queryBuilder = $this->queryBuilderFactory->create();
+        $queryBuilder
+            ->select('ooa.oxartnum, SUM(ooa.oxamount) as sum')
+            ->from('oxorder', 'oo')
+            ->innerJoin('oo', 'oxorderarticles', 'ooa', 'ooa.oxorderid = oo.oxid');
+        ;
+         $dateBetween->addToQuery($queryBuilder, 'oxorderdate');
+
+         $queryBuilder
+            ->groupBy('ooa.oxartnum')
+            ->orderBy('sum', 'DESC')
+            ->setMaxResults($limit)
+        ;
+
+        return $queryBuilder->execute()->fetchAll();
+    }
 }
